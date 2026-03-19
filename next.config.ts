c94c5832cfbb282f11a7978c798b636bc6a25f6d
opervo <opervo.io@gmail.com>
2026-03-19 18:54:18 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/preview-weapon.html',
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
